457ecc119f0290eb9cc26639ec806d8fb711353d
SARTHAK BHATT <sarthakbhatt@SARTHAKs-MacBook-Air.local>
2024-09-24 16:01:08 +0530

    added pro

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRoute = require("./routes/user");
 const propertyRoute = require("./routes/property");
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // file serving
 // Absolute paths for the directories
